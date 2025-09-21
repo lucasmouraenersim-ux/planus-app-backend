62e250ad143678e33f78d3eb02ed24ead868a25f
Lucas de Moura <lucasmouraenersim@gmail.com>
2025-09-21 04:45:18 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 
 import type {NextConfig} from 'next';
-import { withPWA } from "@ducanh2912/next-pwa";
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
